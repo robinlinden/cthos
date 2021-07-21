17b75b068a242c1bd30b350a866bfd1c6aa10042
Robin Linden <dev@robinlinden.eu>
2021-07-22 00:03:08 +0200

    Explicitly tell qemu what our image format is
    qemu gets upset and warns us if we don't do this.

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 set -ex
 
 mkdir -p build
-nasm -o build/boot boot.asm
-qemu-system-x86_64 build/boot
+nasm -o build/boot.bin boot.asm
+qemu-system-x86_64 -drive file=build/boot.bin,index=0,media=disk,format=raw
